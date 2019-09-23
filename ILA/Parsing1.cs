6675981b1dc32478b38b53f8009e6e47a07efd9f
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-09-23 18:35:10 +0200

    starting point for algo parsing

--- ILA/Parsing1.cs
+++ ILA/Parsing1.cs
@@ -10,16 +10,40 @@ namespace ILA
     {
         #region Private Methods
 
+        private static void ParseAlgo(string start, ref int index)
+        {
+        }
+
         private static void ParseFile(string file)
         {
             string fileContent;
             using (var stream = new StreamReader(file))
                 fileContent = stream.ReadToEnd();
+            int index = 0;
+            while (index < fileContent.Length)
+            {
+                if (fileContent.Substring(index, 5) == "algo ")
+                {
+                    var name = ReadAlphaString(fileContent, ref index);
+                    SkipBlankSpaces(fileContent, ref index);
+                }
+            }
+        }
+
+        private static string ReadAlphaString(string str, ref int index)
+        {
+            string result = "";
+            while (index < str.Length && char.IsLetter(str[index]))
+            {
+                result += str[index];
+                index++;
+            }
+            return result;
         }
 
         private static void SkipBlankSpaces(string str, ref int index)
         {
-            var skippedChars = new char[] { ' ', '\t' };
+            var skippedChars = new char[] { ' ', '\t', '\n' };
             while (index < str.Length && skippedChars.Contains(str[index]))
                 index++;
         }
