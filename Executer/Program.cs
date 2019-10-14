c92d2c961966407ae8ba1da9839267d3251f7f6b
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-10-14 21:12:34 +0200

    added executer

---
+++ Executer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Executer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
