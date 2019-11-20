d3234058d8b90540a2831809c0d09e3ab86f2a9a
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-11-20 20:30:50 +0100

    tmp code for tests

--- Executer/Program.cs
+++ Executer/Program.cs
@@ -8,7 +8,14 @@ namespace Executer
 
         private static void Main(string[] args)
         {
-            
+            var prg = new ILANET.Program();
+            prg.Parse(
+@"//test
+algo test
+{
+}"
+);
+            Console.WriteLine(prg.Instructions.Count);
         }
 
         #endregion Private Methods
