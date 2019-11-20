bd3faab8eee22b93ec4fabdc73b27421925a9f7a
OxyTom <oxytom8@gmail.com>
2019-11-20 20:55:16 +0100

    [generator] first variable declaration in python

--- ILA.NET/VariableDeclaration.cs
+++ ILA.NET/VariableDeclaration.cs
@@ -21,8 +21,18 @@ namespace ILANET
 
         internal string LuaCode => throw new NotImplementedException();
 
-        internal string PythonCode => throw new NotImplementedException();
+        internal string PythonCode => GenPythonCode();
         public string Comment { get; set; }
         string IDeclaration.Comment => Comment;
+
+        internal string GenPythonCode()
+        {
+            var SB_var = new StringBuilder().Append(CreatedVariable.Name).Append(" = ");
+            if (CreatedVariable.Constant)
+                SB_var.Append(CreatedVariable.ConstantValue);
+            else
+                SB_var.Append(0);
+            return SB_var.ToString();
+        }
     }
 }
