d3d15415a64536fd966f5b28beba9f1958e70313
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-11-20 15:23:05 +0100

    can no longer change generic types's name

--- ILA.NET/GenericType.cs
+++ ILA.NET/GenericType.cs
@@ -8,16 +8,17 @@ namespace ILANET
     {
         #region Public Fields
 
-        public static readonly GenericType Bool = new GenericType(Flags.BOOL) { Name = "bool" };
-        public static readonly GenericType Char = new GenericType(Flags.CHAR) { Name = "char" };
-        public static readonly GenericType Float = new GenericType(Flags.FLOAT) { Name = "float" };
-        public static readonly GenericType Int = new GenericType(Flags.INT) { Name = "int" };
-        public static readonly GenericType String = new GenericType(Flags.STRING) { Name = "string" };
+        public static readonly GenericType Bool = new GenericType(Flags.BOOL) { name = "bool" };
+        public static readonly GenericType Char = new GenericType(Flags.CHAR) { name = "char" };
+        public static readonly GenericType Float = new GenericType(Flags.FLOAT) { name = "float" };
+        public static readonly GenericType Int = new GenericType(Flags.INT) { name = "int" };
+        public static readonly GenericType String = new GenericType(Flags.STRING) { name = "string" };
 
         #endregion Public Fields
 
         #region Private Constructors
-
+        internal string name;
+        public override string Name { get => name; set => throw new InvalidOperationException("Unable to change the name of a generic type."); }
         private GenericType(Flags type) => Type = type;
 
         #endregion Private Constructors
