7c421334b38f85f73a36c9f5a66c48a19cf6f1a6
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-11-20 13:45:14 +0100

    added white space skipping

--- ILA.NET/Program.cs
+++ ILA.NET/Program.cs
@@ -36,7 +36,30 @@ namespace ILANET
         #endregion Internal Properties
 
         #region Public Methods
-
+        /// <summary>
+        /// Skips every blank character
+        /// </summary>
+        /// <param name="str">string to parse</param>
+        /// <param name="index">index to start from</param>
+        /// <returns>new index after skipping blank spaces</returns>
+        public static int FastForward(string str, int index = 0)
+        {
+            while (index < str.Length && char.IsWhiteSpace(str[index]))
+                index++;
+            return index;
+        }
+        /// <summary>
+        /// Skips blanks characters and line spacing
+        /// </summary>
+        /// <param name="str">string to parse</param>
+        /// <param name="index">index to start from</param>
+        /// <returns>new index after skipping blank spaces/lines</returns>
+        public static int SkipLine(string str, int index = 0)
+        {
+            while (index < str.Length && (char.IsWhiteSpace(str[index]) || str[index] == '\n' || str[index] == '\r'))
+                index++;
+            return index;
+        }
         public string GenerateCode(Language language)
         {
             switch (language)
