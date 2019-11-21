1f738d98440085971afe4bcbb2a46f3454b9f102
OxyTom <oxytom8@gmail.com>
2019-11-21 23:36:57 +0100

    if generator

--- ILA.NET/If.cs
+++ ILA.NET/If.cs
@@ -15,7 +15,21 @@ namespace ILANET
         public List<Instruction> IfInstructions { get; set; }
         string IBaseObject.LuaCode => throw new NotImplementedException();
 
-        string IBaseObject.PythonCode => throw new NotImplementedException();
+        string IBaseObject.PythonCode
+        {
+            get
+            {
+                var sbIF = new StringBuilder().Append("if(");
+                sbIF.Append(IfCondition.PythonCode).Append(")\n");
+                foreach (Instruction instruction in IfInstructions)
+                {
+                    sbIF.Append(instruction.PythonCode + "\n");
+                }
+                // TODO : elif + else
+                return sbIF.ToString();
+            }
+        }
+
         public string Comment { get; set; }
         string Instruction.Comment => Comment;
 
