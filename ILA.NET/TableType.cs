0a2060488da9564fcedbe9316be03bf907982d18
OxyTom <oxytom8@gmail.com>
2019-11-23 23:03:15 +0100

    Wrong class on Table Generator

--- ILA.NET/TableType.cs
+++ ILA.NET/TableType.cs
@@ -42,38 +42,7 @@ namespace ILANET
 
         public override void WritePython(TextWriter textWriter)
         {
-            var indexList = new List<string>();
-
-            // var baseIdent = ident
-            //x .generateIdent()
-            InternalType.WritePython(textWriter);
-            textWriter.Write(" = []\n");
-
-            //x .generateIdent()
-            for (int i = 0; i <= DimensionsSize.Count; i++)
-            {
-                //x .generateIdent()
-                indexList.Add("index" + i);
-                textWriter.Write("for " + indexList[i] + " in range(");
-                DimensionsSize[i].WritePython(textWriter);
-                textWriter.Write("):\n");
-                // ident++
-                //x .generateIdent()
-                InternalType.WritePython(textWriter);
-                if (i < DimensionsSize.Count && i > 0)
-                {
-                    for (int j = 0; j < i; j++)
-                        textWriter.Write("[" + indexList[i - j] + "]");
-                    textWriter.Write(")\n");
-                }
-                textWriter.Write(".append(");
-                if (i < DimensionsSize.Count)
-                    textWriter.Write("[]");
-                else
-                    textWriter.Write(0);
-                textWriter.Write(")\n");
-            }
-            // ident = baseIdent
+            textWriter.Write(Name);
         }
 
         #endregion Public Properties
