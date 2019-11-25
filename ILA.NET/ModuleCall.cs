e61dcf8faf519033840f4268987c41f508e81a33
OxyTom <oxytom8@gmail.com>
2019-11-25 19:15:50 +0100

    argument error fixed

--- ILA.NET/ModuleCall.cs
+++ ILA.NET/ModuleCall.cs
@@ -33,10 +33,13 @@ namespace ILANET
             textWriter.Write("(");
             for (int i = 0; i < Args.Count; i++)
             {
-                if (i != 0)
-                    textWriter.Write(", ");
+                if (Args[i] != null)
+                {
+                    if (i != 0)
+                        textWriter.Write(", ");
 
-                Args[i].WritePython(textWriter);
+                    Args[i].WritePython(textWriter);
+                }
             }
 
             textWriter.Write(")");
