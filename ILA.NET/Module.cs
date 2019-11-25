196016c3dd0e9cf10accb6ac48235c41548e7fbb
OxyTom <oxytom8@gmail.com>
2019-11-25 20:22:15 +0100

    fixed some \n thing

--- ILA.NET/Module.cs
+++ ILA.NET/Module.cs
@@ -72,6 +72,7 @@ namespace ILANET
                             textWriter.Write(", ");
                     }
                 }
+                textWriter.Write("\n");
                 Program.Indent--;
             }
         }
