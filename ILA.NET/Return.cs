7aef561cf4668cd814976c84b4ad522d21d1cd91
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-11-25 22:09:05 +0100

    fixed the shitty merge

--- ILA.NET/Return.cs
+++ ILA.NET/Return.cs
@@ -7,18 +7,17 @@ namespace ILANET
 {
     public class Return : Instruction
     {
-        public IValue Value { get; set; }
         public string Comment { get; set; }
         string Instruction.Comment => Comment;
         public Function Function { get; set; }
-        public IValue Type { get; set; }
+        public IValue Value { get; set; }
 
         public void WriteILA(TextWriter textWriter)
         {
             Program.GenerateIndent(textWriter);
             textWriter.Write(Function.Name);
             textWriter.Write(" <- ");
-            Type.WriteILA(textWriter);
+            Value.WriteILA(textWriter);
             if (Comment != null && Comment.Length > 0)
             {
                 textWriter.Write(" //");
