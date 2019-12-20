29f5cea2273839f124922a9ac52beef626c35747
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-20 14:41:43 +0100

    Create InstructionControl.cs

---
+++ ilaGUI/InstructionControl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ilaGUI
+{
+    internal interface InstructionControl
+    {
+        ILANET.Instruction ManagedInstruction { get; }
+    }
+}
