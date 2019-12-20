69e84f1248f7a7df878747538bc6fba15bcd36e6
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-20 14:50:06 +0100

    added valuecontrol

--- ilaGUI/classes/InstructionControl.cs
+++ ilaGUI/classes/InstructionControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ilaGUI
+namespace ilaGUI.Classes
 {
     internal interface InstructionControl
     {
---
+++ ilaGUI/classes/ValueControl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ilaGUI.Classes
+{
+    internal interface ValueControl
+    {
+        ILANET.IValue ManagedValue { get; }
+    }
+}
