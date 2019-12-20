f891ad5aee7e87cbb071f034e6b2b06e643b9aea
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-20 20:20:53 +0100

    started structure edition

---
+++ ilaGUI/createStruct.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ILANET;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ilaGUI
+{
+    /// <summary>
+    /// Logique d'interaction pour createStruct.xaml
+    /// </summary>
+    public partial class createStruct : Window, Linked
+    {
+        public createStruct()
+        {
+            InitializeComponent();
+        }
+
+        public IBaseObject Link { get; set; }
+    }
+}
