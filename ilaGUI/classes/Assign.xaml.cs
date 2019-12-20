3ec2345be8331ea1d36ab2c5f9e6dfea4912652d
OxyTom <oxytom8@gmail.com>
2019-12-20 09:32:39 +0100

    Added assign class

---
+++ ilaGUI/classes/Assign.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ilaGUI.classes
+{
+    /// <summary>
+    /// Logique d'interaction pour Assign.xaml
+    /// </summary>
+    public partial class Assign : UserControl
+    {
+        public Assign()
+        {
+            InitializeComponent();
+        }
+    }
+}
