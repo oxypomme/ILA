006c9d40236610ac4429832ab81784da7c67b087
OxyTom <oxytom8@gmail.com>
2019-12-20 11:47:17 +0100

    added prototype if uwp control

---
+++ ilaGUI/classes/If.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ilaGUI.Classes
+{
+    /// <summary>
+    /// Logique d'interaction pour If.xaml
+    /// </summary>
+    public partial class If : UserControl
+    {
+        public If()
+        {
+            InitializeComponent();
+        }
+    }
+}
