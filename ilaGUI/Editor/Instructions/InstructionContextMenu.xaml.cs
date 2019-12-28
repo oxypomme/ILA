7df3b17c49a18f8539559eeebee8cec93a1fb622
oxypomme <oxytom8@gmail.com>
2019-12-28 18:47:55 +0100

    Moved InstructionContextMenu to the namespace Editor

--- ilaGUI/Editor/Instructions/InstructionContextMenu.xaml.cs
+++ ilaGUI/Editor/Instructions/InstructionContextMenu.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ilaGUI
+namespace ilaGUI.Editor
 {
     /// <summary>
     /// Logique d'interaction pour InstructionContextMenu.xaml
