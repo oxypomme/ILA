81b99fbbd4eb9e9529a321380961eacc67b3b656
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-30 14:35:34 +0100

    the hsl converter was so big lmao

--- ilaGUI/Utilities.cs
+++ ilaGUI/Utilities.cs
@@ -282,113 +282,66 @@ namespace ilaGUI
             double v;
             hue /= 360;
             double r, g, b;
-
             r = lum;   // default to gray
-
             g = lum;
-
             b = lum;
-
             v = (lum <= 0.5) ? (lum * (1.0 + satur)) : (lum + satur - lum * satur);
-
             if (v > 0)
-
             {
                 double m;
-
                 double sv;
-
                 int sextant;
-
                 double fract, vsf, mid1, mid2;
-
                 m = lum + lum - v;
-
                 sv = (v - m) / v;
-
                 hue *= 6.0;
-
                 sextant = (int)hue;
-
                 fract = hue - sextant;
-
                 vsf = v * sv * fract;
-
                 mid1 = m + vsf;
-
                 mid2 = v - vsf;
-
                 switch (sextant)
-
                 {
                     case 0:
-
                         r = v;
-
                         g = mid1;
-
                         b = m;
-
                         break;
 
                     case 1:
-
                         r = mid2;
-
                         g = v;
-
                         b = m;
-
                         break;
 
                     case 2:
-
                         r = m;
-
                         g = v;
-
                         b = mid1;
-
                         break;
 
                     case 3:
-
                         r = m;
-
                         g = mid2;
-
                         b = v;
-
                         break;
 
                     case 4:
-
                         r = mid1;
-
                         g = m;
-
                         b = v;
-
                         break;
 
                     case 5:
-
                         r = v;
-
                         g = m;
-
                         b = mid2;
-
                         break;
                 }
             }
-
             Color rgb;
-
             rgb.R = Convert.ToByte(r * 255.0f);
-
             rgb.G = Convert.ToByte(g * 255.0f);
-
             rgb.B = Convert.ToByte(b * 255.0f);
             rgb.A = alpha;
             return rgb;
