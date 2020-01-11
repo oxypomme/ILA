b078c93ef59979aa0feb3e9299154cc7d4f04086
OxyTom <oxytom8@gmail.com>
2020-01-11 12:59:41 +0100

    Added a ressource dictionary

---
+++ ilaGUI/CustomWindowStyle.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace ilaGUI
+{
+    public partial class CustomWindowStyle
+    {
+        public CustomWindowStyle()
+        {
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            ((Window)((FrameworkElement)sender).TemplatedParent).Close();
+        }
+
+        private void MaximizeRestoreClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (window.WindowState == WindowState.Normal)
+                window.WindowState = WindowState.Maximized;
+            else
+                window.WindowState = WindowState.Normal;
+        }
+
+        private void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            ((Window)((FrameworkElement)sender).TemplatedParent).WindowState = WindowState.Minimized;
+        }
+    }
+}
